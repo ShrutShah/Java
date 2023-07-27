51922f4a9ffce746be32ed45d5a385754b303795
Shrut Shah <43044788+ShrutShah@users.noreply.github.com>
2023-07-26 21:39:09 -0500

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no: ");
+        int a = sc.nextInt();
+        System.out.println(a);
+        
+        
+        
+        
+    }
+}
